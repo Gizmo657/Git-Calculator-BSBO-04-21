08ba2b919c56b36bf5fd3acc4e0766b5d8dfa20c
student <student@C349U17>
2024-03-16 11:42:05 +0300

    FIRST

--- Git Calculator BSBO-04-21/Form1.cs
+++ Git Calculator BSBO-04-21/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace Git_Calculator_BSBO_04_21
 {
-    public partial class Form1 : Form
+    public partial class Calcultor : Form
     {
-        public Form1()
+        public Calcultor()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ;
         }
     }
 }
--- Git Calculator BSBO-04-21/Program.cs
+++ Git Calculator BSBO-04-21/Program.cs
@@ -16,7 +16,7 @@ namespace Git_Calculator_BSBO_04_21
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Calcultor());
         }
     }
 }
